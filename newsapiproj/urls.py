383c36ecae1e8ded272f77a7c8144b65cbcb02fd
khatrisagar <khatrisagar404@gmail.com>
2022-10-15 15:02:46 +0530

    small changes added

--- newsapiproj/urls.py
+++ newsapiproj/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path,include
 from .views import mainView
 from django.conf.urls.static import static
+
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('',mainView.as_view(), name='mainview'),
+    path('',mainView.as_view(), name='mainiew'),
 
-]
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
