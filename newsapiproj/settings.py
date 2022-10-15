acd32a0647b52427020a270ae237a34dfc41a0aa
khatrisagar <khatrisagar404@gmail.com>
2022-10-15 15:31:41 +0530

    small chane added

--- newsapiproj/settings.py
+++ newsapiproj/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS =(os.path.join(BASE_DIR / 'static'),)
 WHITENOISE_USE_FINDERS = True
 
 
-MEDIA_URL = 'https://newsapi-sagar.herokuapp.com/images/'
+MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
